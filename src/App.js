5758d832292f1fe5eb54e46bdd360aa16d9368b4
Will Adams <bushblade@gmail.com>
2018-11-12 20:45:02 +0000

    refactor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
+import { Transition, animated } from 'react-spring'
 
+// components
 import Box from './Box'
 import Controls from './Controls'
 
+// helpers
 import createBoxes from './helpers/createBoxes'
 import shuffle from './helpers/shuffle'
 
@@ -42,14 +45,28 @@ class App extends Component {
       deleteBox,
       state: { filtered }
     } = this
+
     return (
       <div className="App">
         <Controls filter={filterBy} reset={resetColors} shuffleBoxes={shuffleBoxes} />
         <div className="container">
           <div className="boxes-wrapper">
-            {filtered.map(({ id, color }) => (
-              <Box key={id} color={color} id={id} deleteBox={deleteBox} />
-            ))}
+            <Transition
+              native
+              items={filtered}
+              keys={item => item.id}
+              unique={true}
+              reset={true}
+              from={{ opacity: 0, height: 0, width: 0 }}
+              enter={{ opacity: 1, height: 'auto', width: 'auto' }}
+              leave={{ opacity: 0 }}
+              trail={10}>
+              {item => props => (
+                <animated.div style={{ ...props }}>
+                  <Box color={item.color} id={item.id} deleteBox={deleteBox} />
+                </animated.div>
+              )}
+            </Transition>
           </div>
         </div>
       </div>
